5551e922f8717456134ac6572c9a4dfaff4cb505
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2021-09-28 14:54:54 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+//Header fixed
+
+window.onscroll = function () {
+  const docScrollTop = document.documentElement.scrollTop;
+
+  if (window.innerWidth > 991) {
+    if (docScrollTop > 100) {
+      document.querySelector("header").classList.add("fixed");
+    } else {
+      document.querySelector("header").classList.remove("fixed");
+    }
+  }
+}
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function (element) {
+  element.addEventListener("click", function () {
+    for (let i = 0; i < a.length; i++) {
+      a[i].classList.remove("active")
+    }
+    this.classList.add("active");
+    document.querySelector(".navbar").classList.toggle("show");
+  })
+})
+
+//Hamburger
+
+const hamBurger = document.querySelector(".hamburger");
+
+hamBurger.addEventListener("click", function () {
+  document.querySelector(".navbar").classList.toggle("show");
+})
+
+//Portfolio Gallery
+
+const filterBtn = document.querySelector("#filterBtn").children;
+const item = document.querySelector(".gallery").children;
+
+for (let i = 0; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener("click", function () {
+    for (let j = 0; j < filterBtn.length; j++) {
+      filterBtn[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target");
+    for (let k = 0; k < item.length; k++) {
+      item[k].style.display = "none";
+      if (target == item[k].getAttribute("data-id")) {
+        item[k].style.display = "block";
+      }
+      if (target == "all") {
+        item[k].style.display = "block";
+      }
+    }
+  });
+}
+
+
